e3ed30f77f8a3a5350af078b2be7e0228bf0557c
hid3h <razyetts@hotmail.co.jp>
2022-11-01 23:14:13 +0900

    cloud runでログ見たいので

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const test = async (req: NextApiRequest, res: NextApiResponse) => {
-  
+  console.log("req.headers", req.headers)
+  console.log("req.method", req.method)
+  console.log("req.body", req.body)
+
   res.status(200).json({tes: "ok"});
 };
 
