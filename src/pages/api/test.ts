6e49ce63865a30fbee4ac65fe5f908c567a05b4a
hide <razyetts@hotmail.co.jp>
2022-11-02 10:44:30 +0900

    cloud runでのログを見やすく

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const test = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("req.headers", req.headers)
-  console.log("req.method", req.method)
-  console.log("req.body", req.body)
+  const entry: any = {
+    headers: req.headers,
+    method: req.method,
+    body: req.body
+  }
+  console.log(JSON.stringify(entry))
 
   res.status(200).json({tes: "ok"});
 };
