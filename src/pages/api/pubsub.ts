68c083c0b13c19c4ba20f4b44c86c6e7e251ba3a
hide <razyetts@hotmail.co.jp>
2022-11-02 11:09:23 +0900

    GETでチャレンジを返さないと行けなかったのとapi変更

---
+++ src/pages/api/pubsub.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const pubsub = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req
+
+  // TODO: リクエスト検証
+
+  if (method == "GET") {
+    res.status(200).send(req.query["hub.challenge"])
+  }
+
+  if (method != "POST") {
+    res.status(404)
+  }
+
+  const entry: any = {
+    headers: req.headers,
+    method: req.method,
+    body: req.body
+  }
+  console.log(JSON.stringify(entry))
+
+  res.status(200).json({tes: "ok"});
+};
+
+export default pubsub;
